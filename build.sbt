31f46d116ecac716a80ae06ccc4d420ce4b3ec05
Andy Marke <andy.marke@gmail.com>
2019-08-28 09:58:09 +0100

    685 optionally sign requests to elastic search so it can be secured in AWS

--- build.sbt
+++ build.sbt
@@ -17,6 +17,7 @@ libraryDependencies ++= Seq(
   "ch.qos.logback" % "logback-classic" % "1.2.3",
   "org.apache.logging.log4j" % "log4j-to-slf4j" % "2.9.1",
   "org.elasticsearch.client" % "elasticsearch-rest-high-level-client" % "6.1.1",
+  "com.amazonaws" % "aws-java-sdk" % "1.11.46",
 
   "org.scalatest" %% "scalatest" % "3.0.4" % "test",
   "org.mockito" % "mockito-core" % "2.13.0" % "test",
--- src/main/scala/gov/uk/justice/digital/offenderpollpush/injection/RestHighLevelClientProvider.scala
+++ src/main/scala/gov/uk/justice/digital/offenderpollpush/injection/RestHighLevelClientProvider.scala
@@ -1,9 +1,27 @@
 package gov.uk.justice.digital.offenderpollpush.injection
 
+import com.amazonaws.auth.{AWS4Signer, DefaultAWSCredentialsProviderChain}
+import com.google.inject.name.Named
 import com.google.inject.{Inject, Provider}
+import gov.uk.justice.digital.offenderpollpush.helpers.AWSRequestSigningApacheInterceptor
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
 import org.elasticsearch.client.{RestClientBuilder, RestHighLevelClient}
 
-class RestHighLevelClientProvider @Inject() (builder: RestClientBuilder) extends Provider[RestHighLevelClient] {
+class RestHighLevelClientProvider @Inject() (builder: RestClientBuilder,
+                                            @Named("signSearchRequests") signSearchRequests: Boolean,
+                                            @Named("searchAWSRegion") searchAWSRegion: String,
+                                            @Named("searchAWSServiceName") searchAWSServiceName: String) extends Provider[RestHighLevelClient] {
+  private val credentialsProvider = new DefaultAWSCredentialsProviderChain
 
-  override def get: RestHighLevelClient = new RestHighLevelClient(builder)
+  override def get: RestHighLevelClient = {
+    if (signSearchRequests) {
+      val signer = new AWS4Signer
+      signer.setServiceName(searchAWSServiceName)
+      signer.setRegionName(searchAWSRegion)
+
+      new RestHighLevelClient(builder.setHttpClientConfigCallback((callback: HttpAsyncClientBuilder) => callback.addInterceptorLast(new AWSRequestSigningApacheInterceptor(searchAWSServiceName, signer, credentialsProvider))))
+    } else {
+      new RestHighLevelClient(builder)
+    }
+  }
 }
